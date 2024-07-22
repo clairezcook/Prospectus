59338827614d95711e3af94da7a4f60ee4dc0e27
Claire <ccz99536@uga.edu>
2024-07-22 15:28:28 -0400

    Change to make the fv/fm plot the same as the other 2

--- CultureHistorical.R
+++ CultureHistorical.R
@@ -35,8 +35,7 @@ size <- ggplot(df_aov, aes(x= species, y = emmean, fill = factor(treatment, leve
   geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL), position = position_dodge(.9), width = 0.2) + 
   geom_text(aes(label = cld, y = upper.CL), vjust = -0.5, position = position_dodge(0.9),size = 3)+
   labs(x="Culture", y="Diameter (µm)", fill="Media Type", title="b)")+
-  theme_bw() + scale_fill_manual(values=colors)+
-  theme(legend.position = 'none')
+  theme_bw() + scale_fill_manual(values=colors)
 
 #### Growth Rates ####
 setwd("~/Library/CloudStorage/OneDrive-UniversityofGeorgia/Lab/prelimMIXO/Growth/ChelexedGS_Raw")
@@ -65,21 +64,19 @@ mixo <- ggplot(df_aov, aes(x= species, y = emmean, fill = factor(treatment, leve
   geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL), position = position_dodge(.9), width = 0.2) + 
   geom_text(aes(label = cld, y = upper.CL), vjust = -0.5, position = position_dodge(0.9),size = 3)+
   labs(x="Culture", y="Growth Rate (1/day)", fill="Media Treatment", title="a)")+
-  theme_bw()+  scale_fill_manual(values=colors)+
-  theme(legend.position = 'none')
+  theme_bw()+  scale_fill_manual(values=colors)
 mixo
 
 #### Fv/Fm ####
-## GC (From GC June file)
-order <- c('Replete', 'Low Mn', 'Low Fe', 'Low Fe, Mn')
-gc_fv <- read_excel("~/Desktop/OneDrive/Lab/prelimMIXO/FIRe/JuneGC/JuneGC.xlsx")
-colnames(gc_fv)[6]='PAR'
-colnames(gc_fv)[12]='FvFm'
+colors <- c("Low Fe" = "gray30", "Low Fe, Mn" = "black", "Replete" = "white","Low Mn" = "gray80")
 
+setwd("~/Desktop/OneDrive/Lab/prelimMIXO/FIRe")
+fvfm <- read_excel("HistoricalFvFm.xlsx")
 
-data <- data.frame(treatment = gc_fv$Media, result = gc_fv$FvFm)
-
+# Perform ANOVA and post-hoc tests
+data <-  data.frame (species = fvfm$Culture, treatment = fvfm$Media, result = fvfm$`Fv/Fm`)
 df_aov <- data %>%
+  dplyr::group_by(species) %>%
   tidyr::nest() %>%
   rowwise() %>% 
   dplyr::mutate(aov_results = list(aov(result ~ treatment, data = data)), 
@@ -90,76 +87,19 @@ df_aov <- data %>%
   dplyr::mutate(cld = trimws(.group)) %>% 
   dplyr::select(-.group)
 
-gcfv <- ggplot(df_aov, aes(x=factor(treatment, levels=order), y = emmean, fill = factor(treatment, levels =order)))+
-  geom_bar(position = 'dodge', stat = 'identity', color="black")+
-  geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL), position = position_dodge(.9), width = 0.2) + 
-  geom_text(aes(label = cld, y = upper.CL), vjust = -0.5, position = position_dodge(0.9),size = 3)+
-  labs(y="Fv/Fm", x="",fill="Media Type", title="c) G. cryophilia")+
-  theme_bw()+ scale_fill_manual(values=colors)+scale_y_continuous(limits=c(0,0.6))+
-  theme(legend.position = 'none', plot.margin = unit(c(0.25,3.35,0.25,0.25), "cm"))
-gcfv
+# Plot the combined data
+fvfm <- ggplot(df_aov, aes(x = species, y = emmean, fill = factor(treatment, levels = order))) +
+  geom_bar(position = 'dodge', stat = 'identity', color = "black") +
+  geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL), position = position_dodge(0.9), width = 0.2) +
+  geom_text(aes(label = cld, y = upper.CL), vjust = -0.5, position = position_dodge(0.9), size = 3) +
+  labs(x = "Culture", y = "Fv/Fm", fill = "Media Treatment", title="c)") +
+  theme_bw() + scale_fill_manual(values = colors)
 
-## MA
-ma_fv <- read_excel("~/Desktop/OneDrive/Lab/prelimMIXO/FIRe/JuneMA/JuneMA.xlsx")
-colnames(ma_fv)[6]='PAR'
-colnames(ma_fv)[11]='FvFm'
-ma_fv <- ma_fv[-4,]
-data <- data.frame(treatment = ma_fv$Media, result = ma_fv$FvFm)
+print(fvfm)
 
-df_aov <- data %>%
-  tidyr::nest() %>%
-  rowwise() %>% 
-  dplyr::mutate(aov_results = list(aov(result ~ treatment, data = data)), 
-                emm = list(emmeans::emmeans(aov_results, "treatment", type= "response")), 
-                cld = list(multcomp::cld(emm, Letters = LETTERS, reverse = TRUE))) %>% 
-  dplyr::select(-data, -aov_results, -emm) %>% 
-  unnest(cols = c(cld)) %>%
-  dplyr::mutate(cld = trimws(.group)) %>% 
-  dplyr::select(-.group)
 
-order <- c("Replete", "Low Mn", "Low Fe", "Low Fe, Mn")
-mafv <- ggplot(df_aov, aes(x=factor(treatment, levels=order), y = emmean, fill = factor(treatment, levels=order)))+
-  geom_bar(position = 'dodge', stat = 'identity', color="black")+
-  geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL), position = position_dodge(.9), width = 0.2) + 
-  geom_text(aes(label = cld, y = upper.CL), vjust = -0.5, position = position_dodge(0.9),size = 3)+
-  labs(y="Fv/Fm", x="",fill="Media Type", title="M. Antarctica")+
-  theme_bw() + scale_fill_manual(values=colors) + scale_y_continuous(limits=c(-0.04,0.25))
-mafv
-
-## PT (from PT_ETR.R in ETR folder)
-setwd("~/Desktop/OneDrive/Lab/prelimMIXO/FIRe/ETR")
-order <- c('Replete', 'Low Mn', 'Low Fe', 'Low Fe, Mn')
-ptfv <- read_excel("PT_ETR.xlsx", sheet="Fv")
-colnames(ptfv)[5]='PAR'
-colnames(ptfv)[11]='FvFm'
-
-data <- data.frame(treatment = ptfv$Media, result = ptfv$FvFm)
-
-df_aov <- data %>%
-  tidyr::nest() %>%
-  rowwise() %>% 
-  dplyr::mutate(aov_results = list(aov(result ~ treatment, data = data)), 
-                emm = list(emmeans::emmeans(aov_results, "treatment", type= "response")), 
-                cld = list(multcomp::cld(emm, Letters = LETTERS, reverse = TRUE))) %>% 
-  dplyr::select(-data, -aov_results, -emm) %>% 
-  unnest(cols = c(cld)) %>%
-  dplyr::mutate(cld = trimws(.group)) %>% 
-  dplyr::select(-.group)
-
-ptfv <- ggplot(df_aov, aes(x=factor(treatment, levels=order), y = emmean, fill = factor(treatment, levels =order)))+
-  geom_bar(position = 'dodge', stat = 'identity', color="black")+
-  geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL), position = position_dodge(.9), width = 0.2) + 
-  geom_text(aes(label = cld, y = upper.CL), vjust = -0.5, position = position_dodge(0.9),size = 3)+
-  labs(y="Fv/Fm", x="Media Type",fill="Media Type", title="P. tychotreta")+
-  theme_bw() + scale_fill_manual(values=colors) + scale_y_continuous(limits=c(0,0.6)) +
-  theme(legend.position = 'none', plot.margin = unit(c(0.25,3.35,0.25,0.25), "cm"))
-ptfv
 
 library(gridExtra)
-allfv <- grid.arrange(gcfv, mafv, ptfv)
-combined_plot <- grid.arrange(
-  arrangeGrob(mixo, size, ncol = 1),
-  allfv,
-  widths = c(1, 2)
-)
+combined_plot <- grid.arrange(mixo, size, fvfm)
 
+                              
